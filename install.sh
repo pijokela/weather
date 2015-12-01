ec2146fe33bc4de30a91d4ae88a4392134da8ecc
Pirkka Jokela <pirkka.jokela@iki.fi>
2015-12-01 20:27:34 +0200

    install script

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ sudo stop weather
 ./activator stage
 sudo rm -rf /opt/weather-0.1
 sudo cp -r target/universal/stage /opt/weather-0.1
+sudo chown -R pi:users /opt/weather-0.1
 sudo start weather
 sudo tail -f /var/log/upstart/weather.log
