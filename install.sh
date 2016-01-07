473f2d7a5f4dd6962fe6bb6b4fb6981b79ba25a1
Pirkka Jokela <pirkka.jokela@iki.fi>
2016-01-07 21:17:08 +0200

    Fix install

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo stop weather
 ./activator stage
 sudo rm -rf /opt/weather-0.1
 sudo cp -r target/universal/stage /opt/weather-0.1
+sudo cp weather.conf /etc/init/
 sudo chown -R pi:users /opt/weather-0.1
 sudo start weather
 sudo tail -f /var/log/upstart/weather.log
