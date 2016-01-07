7a4110ff92456e87d75ce38f79a8760af55e1a2d
Pirkka Jokela <pirkka.jokela@iki.fi>
2016-01-07 22:43:43 +0200

    Fix install

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo stop weather
 sudo rm -rf /opt/weather-0.1
 sudo cp -r target/universal/stage /opt/weather-0.1
 sudo cp weather.conf /etc/init/
+sudo cp $1 /opt/weather-0.1/conf/application.conf
 sudo chown -R pi:users /opt/weather-0.1
 sudo start weather
 sudo tail -f /var/log/upstart/weather.log
